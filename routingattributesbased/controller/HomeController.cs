22d12f6227e73de505a0c2f3febf9678d0ad7446
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-07-28 10:11:18 -0700

    Initial commit

---
+++ routingattributesbased/controller/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace routingattributesbased.controller
+{
+    //public class HomeController : Controller
+    //{
+    //    [Route("")]
+    //    [Route("Home")]
+    //    [Route("Home/Index")]
+
+    //    public IActionResult Index()
+    //    {
+    //        return View();
+    //    }
+
+    //    [Route("Home/About")]
+    //    public IActionResult About()
+    //    {
+    //        return View();
+    //    }
+
+    //    [Route("Home/GetEmpById/{Id?}")]
+    //    public int GetEmpById(int? Id)
+    //    {
+    //        return Id?? 1 ;
+    //    }
+
+    //}
+
+    
+
+    [Route("Home")]
+   
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        [Route("")]
+        [Route("Index")]
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("GetEmpById/{Id?}")]
+        public int GetEmpById(int? Id)
+        {
+            return Id ?? 1;
+        }
+
+    }
+}
